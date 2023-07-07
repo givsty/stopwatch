34152bc050cb74c10a510ded5ff3a073c5ff6d94
givsty <golovkovvigg@gmail.com>
2023-07-07 15:00:48 +0300

    new commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const start = document.querySelector('.start')
 const timeSec = document.querySelector('.timer__inner__sec')
 const timeMin = document.querySelector('.timer__inner__min')
 const timeMsec = document.querySelector('.timer__inner__msec')
+const timeDec = document.querySelector('.timer__inner__decor')
 const pause = document.querySelector('.pause')
 const save = document.querySelector('.save')
 const saving = document.querySelector('.savig')
@@ -9,8 +10,14 @@ const saving = document.querySelector('.savig')
 let i = 0
 let j = 0
 let k = 0
+let m = 0
 
 start.addEventListener('click', ()=> {
+  const timerDec = setInterval(()=> {
+    timeDec.textContent = ++m;
+    m > 8 ? m = 0 : ''
+  }, )
+
   const timerMsec = setInterval(()=> {
     timeMsec.textContent = ++k
     k > 8 ? k = -1: ''
@@ -29,10 +36,13 @@ start.addEventListener('click', ()=> {
     clearInterval(timerMsec)
     clearInterval(timerSec)
     clearInterval(timerMin)
+    clearInterval(timerDec)
   })
 
   save.addEventListener('click', ()=>{
-    saving.textContent = i
+    for(let l = 0; l < 3; l++) {
+      saving.innerHTML = `${i}:${j}:${k}`
+    }
   })
 
 })
